4122e9959f48fa558007d121c53e9f5d5269a036
RithikBanerjee <banerjee13rithik@gmail.com>
2020-07-19 18:00:10 -0700

    [feature] Added scheduled deletion of old notifications

--- tests/openwisp2/celery.py
+++ tests/openwisp2/celery.py
@@ -3,9 +3,21 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openwisp2.settings')
 
 app = Celery('openwisp2')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+"""
+Delete notifications having 'timestamp' more than 90 days, task scheduled for every Monday 2:00 a.m.
+"""
+app.conf.beat_schedule = {
+    'delete-expired-notifications-every-monday': {
+        'task': 'tasks.add',
+        'schedule': crontab(hour=2, day_of_week=1),
+        'args': (90,)
+    },
+}
+app.conf.timezone = 'UTC'
 app.autodiscover_tasks()
