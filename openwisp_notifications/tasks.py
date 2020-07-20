600598c0b73e4046c16a9db123dce41f8dc74f90
RithikBanerjee <banerjee13rithik@gmail.com>
2020-07-20 10:26:57 -0700

    [feature] Added scheduled deletion of old notifications

--- openwisp_notifications/tasks.py
+++ openwisp_notifications/tasks.py
@@ -2,6 +2,8 @@ from celery import shared_task
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
 from openwisp_notifications.swapper import load_model
+from django.util import timezone
+from datetime import timedelta
 
 Notification = load_model('Notification')
 
@@ -23,5 +25,16 @@ def delete_obsolete_notifications(instance_app_label, instance_model, instance_i
         Q(actor_object_id=instance_id)
         | Q(action_object_object_id=instance_id)
         | Q(target_object_id=instance_id)
+    )
+    Notification.objects.filter(where).delete()
+
+
+@shared_task
+def delete_old_notifications(days):
+    """
+    Delete notifications having 'timestamp' more than 90 days, task scheduled for every day.
+    """
+    where = (
+        Q(timestamp=timezone.now() - timedelta(days=days))
     )
     Notification.objects.filter(where).delete()
