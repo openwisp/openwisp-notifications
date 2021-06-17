53ed629e08a306130765ac4c556b83ec76fd648a
Nitesh Sinha <niteshsinha1707@gmail.com>
2021-06-17 18:35:18 +0530

    [chores] Minor changes

--- openwisp_notifications/tests/test_utils.py
+++ openwisp_notifications/tests/test_utils.py
@@ -38,11 +38,10 @@ class TestChecks(TestCase, TestOrganizationMixin):
     @patch.object(
         app_settings, 'OPENWISP_NOTIFICATIONS_HOST', 'https://example.com',
     )
-    @patch('openwisp_utils.admin_theme.menu.MENU', {})
     def test_cors_not_configured(self):
         # If INSTALLED_APPS not configured
-        with patch(
-            'openwisp_notifications.types.NOTIFICATION_TYPES', dict()
+        with patch('openwisp_notifications.types.NOTIFICATION_TYPES', dict(),), patch(
+            'openwisp_utils.admin_theme.menu.MENU', {}
         ), self.modify_settings(
             INSTALLED_APPS={'remove': 'corsheaders'}
         ), StringIO() as stderr:
