f2da95b9271726af023f75135ceaf726fe6723e2
Dhanus <dhanus3133@gmail.com>
2024-07-17 20:28:32 +0530

    [refactor] URL routes

--- openwisp_notifications/api/urls.py
+++ openwisp_notifications/api/urls.py
@@ -9,31 +9,40 @@ def get_api_urls(api_views=None):
     if not api_views:
         api_views = views
     return [
-        path('', views.notifications_list, name='notifications_list'),
-        path('read/', views.notifications_read_all, name='notifications_read_all'),
-        path('<uuid:pk>/', views.notification_detail, name='notification_detail'),
+        path('notification/', views.notifications_list, name='notifications_list'),
         path(
-            '<uuid:pk>/redirect/',
+            'notification/read/',
+            views.notifications_read_all,
+            name='notifications_read_all',
+        ),
+        path(
+            'notification/<uuid:pk>/',
+            views.notification_detail,
+            name='notification_detail',
+        ),
+        path(
+            'notification/<uuid:pk>/redirect/',
             views.notification_read_redirect,
             name='notification_read_redirect',
         ),
+        # WIP
+        # path(
+        #     'user/user-setting/',
+        #     views.notification_setting_list,
+        #     name='notification_setting_list',
+        # ),
+        # path(
+        #     'user/user-setting/<uuid:pk>/',
+        #     views.notification_setting,
+        #     name='notification_setting',
+        # ),
         path(
-            'user-setting/',
-            views.notification_setting_list,
-            name='notification_setting_list',
-        ),
-        path(
-            'user-setting/<uuid:pk>/',
-            views.notification_setting,
-            name='notification_setting',
-        ),
-        path(
-            'ignore/',
+            'notification/ignore/',
             views.ignore_object_notification_list,
             name='ignore_object_notification_list',
         ),
         path(
-            'ignore/<str:app_label>/<str:model_name>/<uuid:object_id>/',
+            'notification/ignore/<str:app_label>/<str:model_name>/<uuid:object_id>/',
             views.ignore_object_notification,
             name='ignore_object_notification',
         ),
