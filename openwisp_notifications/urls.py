aca081a91b590ec5ba06e9c9f8e73a419a7a7479
Dhanus <dhanus3133@gmail.com>
2024-08-01 19:20:39 +0530

    [feat] Notification Settings Page

--- openwisp_notifications/urls.py
+++ openwisp_notifications/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 
 from .api.urls import get_api_urls
+from .base.views import notifiation_setting_page
 
 
 def get_urls(api_views=None, social_views=None):
@@ -9,7 +10,11 @@ def get_urls(api_views=None, social_views=None):
     Arguments:
         api_views(optional): views for Notifications API
     """
-    urls = [path('api/v1/notifications/', include(get_api_urls(api_views)))]
+    urls = [
+        path('api/v1/notifications/', include(get_api_urls(api_views))),
+        path('notifications/settings/', notifiation_setting_page),
+        path('notifications/user/<uuid:pk>/settings/', notifiation_setting_page),
+    ]
     return urls
 
 
