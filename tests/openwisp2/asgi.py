a53a0dfc85dd3c09d30c1bc4033fd6cd00412856
Gagan Deep <pandafy.dev@gmail.com>
2025-04-05 03:04:16 +0530

    [deps] Add support for Django 5.x and update channels #340
    - Dropped support for Python < 3.9
    - Dropped support for Django < 4.2
    
    Closes #340

--- tests/openwisp2/asgi.py
+++ tests/openwisp2/asgi.py
@@ -2,6 +2,7 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 
 from openwisp_notifications.websockets.routing import get_routes
 
@@ -11,13 +12,18 @@ if os.environ.get('SAMPLE_APP', False):
     # the web socket consumer in your extended app.
     from .sample_notifications import consumers
 
-    application = ProtocolTypeRouter(
-        {'websocket': AuthMiddlewareStack(URLRouter(get_routes(consumers)))}
-    )
+    routes = get_routes(consumers)
+
 else:
     # Load openwisp_notifications consumers:
     # This can be used when you are extending the app but not making
     # any changes in the web socket consumer.
-    application = ProtocolTypeRouter(
-        {'websocket': AuthMiddlewareStack(URLRouter(get_routes()))}
-    )
+    routes = get_routes()
+
+
+application = ProtocolTypeRouter(
+    {
+        'websocket': (AuthMiddlewareStack(URLRouter(routes))),
+        'http': get_asgi_application(),
+    }
+)
